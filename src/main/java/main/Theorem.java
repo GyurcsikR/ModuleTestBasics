df2ef56694f701c0d4e90a3ce7a8d26b66c580bc
GyurcsikR <gyurcsikr@gmail.com>
2022-04-09 09:27:22 +0200

    elmélet

--- src/main/java/main/Theorem.java
+++ src/main/java/main/Theorem.java
@@ -8,16 +8,21 @@ public class Theorem {
 
     /* Milyen primitív adattípusok léteznek a Java-ban? Sorold fel őket! - 1p
         Ide jön a válaszod...
+        Int, byte, short, long, double, float, char, boolean.
      */
 
 
     /* Mi a különbség a while és a do-while ciklusok között? - 2p
         Ide jön a válaszod...
+        Az egyik elöltesztelő, a másik pedig hátultesztelő típusú. A do-while mindenféleképpen egyszer lefut, ha teljesül a feltétel akkor folytatódik, ez a hátultesztelő.
      */
 
 
     /* Mik a tömbök? (Mire használhatóak? Mikor érdemes használni őket? Hogyan hozhatjuk létre őket?) - 3p
         Ide jön a válaszod...
+        A NEM primitív adattipusokhoz tartozik, egyszerű adattípusokat tárolhatunk benne, több dimenziós is lehet.
+        int[] tömb = {};
+        int[] tömb = new int[0];
      */
 
 }
